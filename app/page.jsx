adadaa2be361d30ccf9bb26db01d86a014d64101
ell-ska <ella@skagerstrom.se>
2023-03-20 15:23:33 +0100

    remove tags

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,6 @@ export default function Home() {
   return (
     <div>
       First page of website
-      <div className="tag">Körkort</div>
-      <div className="tag disabled">Körkort</div>
-      <div className="tag green">Körkort</div>
     </div>
   )
 }

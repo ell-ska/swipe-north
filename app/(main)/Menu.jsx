22659f9825fe98177d7c2002368d17687f830f9b
idaakermark <ida.akermark@edu.cmeducations.se>
2023-03-20 20:38:41 +0100

    finished menu

--- app/(main)/Menu.jsx
+++ app/(main)/Menu.jsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import '../globalStyles/components/Menu.css'
 
 export default function Menu() {
     return (
         <>
         <div className='menu-items'>
-            <Link href='/home'>Hem</Link>         
-            <Link href='/saved-jobs'>Sparade jobb</Link>
-            <Link href='/profile'>Mina taggar</Link>     
+            <div className='home-link'>
+                <Link href='/home'>Hem</Link>
+            </div>         
+            <div className='saved-link'>
+                <Link href='/saved-jobs'>Sparade jobb</Link>
+            </div>
+            <div className='tags-link'>
+                <Link href='/profile'>Mina taggar</Link>
+            </div>     
         </div>                            
        
         <div className='menu-container'>            

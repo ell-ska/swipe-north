0b810afa034235816c7d19c5682a60f29a47d977
ell-ska <ella@skagerstrom.se>
2023-03-20 17:08:20 +0100

    add button components

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import './globalStyles/variables.css'
 import './globalStyles/globals.css'
 import './globalStyles/typography.css'
 import './globalStyles/components/tags.css'
+import './globalStyles/components/buttons.css'
 
 export default function Home() {
   return (

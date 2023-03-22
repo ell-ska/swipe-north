abf5de7055a8c3aafcfdf4d22610d0dda7efcc9c
Max Häggman <max.haggman@edu.cmeducations.se>
2023-03-22 12:40:42 +0100

    login page style/layout

--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,23 @@ import './globalStyles/globals.css'
 import './globalStyles/typography.css'
 import './globalStyles/components/tags.css'
 import './globalStyles/components/buttons.css'
+import logo from '../public/icons/skelleftea-logo.svg'
+import Image from 'next/image'
+import styles from '../app/globalStyles/components/login.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Login() {
   return (
-    <div>
-      First page of website
-      <div>
-        <Tag color='orange' isActive={true}>Test</Tag>
-        <Tag color='green' isActive={false}>Test</Tag>
-      </div>
+    <div className={styles.loginComponent}>
+        <div  className={styles.image}>
+          <Image src={logo} alt='Skelleftea logo' width={125} height={67}/>
+        </div>
+        <div className={styles.loginContainer}>
+            <Link href='/home'>
+              <button className={`button ${styles.button}`}>Logga in</button>
+            </Link>
+            <button className={styles.createAccount}>Eller skapa ett konto</button>
+        </div>
     </div>
   )
 }

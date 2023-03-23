76f5ba4ba0c7a2b2213195814fa094bdcda161ac
idaakermark <ida.akermark@edu.cmeducations.se>
2023-03-23 11:19:30 +0100

    job cards layout

---
+++ app/(main)/saved-jobs/Jobcards.jsx
@@ -0,0 +1,17 @@
+import '../../globalStyles/components/JobCards.css'
+import taxi from '../../../public/images/jobs/taxi-mobile.jpg'
+import Image from 'next/image'
+
+export default function JobCards() {
+    return (
+        <div className='job-card-container'>
+            <div className='jobcard'>
+                <div className='job-card-image' style={{backgroundImage: `url(${taxi.src})`}}></div>
+                <div className='card-description'>
+                    <p className='company-name'>Skellefteå Taxi</p>
+                    <p className='job-title'>Taxichaufför</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/(main)/saved-jobs/page.jsx
+++ app/(main)/saved-jobs/page.jsx
@@ -1,6 +1,10 @@
+import JobCards from "./Jobcards"
+
 export default function SavedJobs() {
     return (
        <>
+       <h3 className='saved-jobs-title'>Sparade jobb</h3>
+       <JobCards></JobCards>
             
        </>
     )

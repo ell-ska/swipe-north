dc40a85e004efc286b64932e199096e508c6d537
idaakermark <ida.akermark@edu.cmeducations.se>
2023-03-27 14:57:25 +0200

    link to see-more page

--- app/(main)/saved-jobs/page.jsx
+++ app/(main)/saved-jobs/page.jsx
@@ -1,12 +1,35 @@
-import JobCards from "./Jobcard"
+import Link from 'next/link'
+import JobCards from './Jobcard'
+
+export default function SavedJobs() {
+  return (
+    <>
+      <h3 className='saved-jobs-title'>Sparade jobb</h3>
+      <div className='job-card-container'>
+        <Link href='/saved-jobs/see-more'>
+          
+            <JobCards />
+          
+        </Link>
+      </div>
+    </>
+  );
+}
+
+
+
+
+/*import JobCards from "./Jobcard"
+import Link from 'next/link';
 
 export default function SavedJobs() {
     return (
        <>
         <h3 className='saved-jobs-title'>Sparade jobb</h3>
         <div className='job-card-container'>
-            <JobCards></JobCards>
+            <JobCards></JobCards> 
        </div>
        </>
     )
 }
+*/

a9005a8293d848389d097142911397ca2182ea61
ell-ska <ella@skagerstrom.se>
2023-03-28 12:19:53 +0200

    create swipe buttons component

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,11 @@
 'use client'
 import { useState } from 'react'
-import Image from 'next/image'
 import Header from './Header'
+import SwipeButtons from './SwipeButtons'
 import api from './dummyApi'
 import './globalStyles/home.css'
 import './globalStyles/components/buttons.css'
-import heartIcon from 'public/icons/heart-outlined.svg'
-import returnIcon from 'public/icons/return.svg'
-import crossIcon from 'public/icons/cross.svg'
 import SwipeCard from './SwipeCard'
-
-export function Buttons() {
-    return (
-        <div className="buttons">
-            <button className="button icon">
-                <Image src={returnIcon} alt="Ångra"></Image>
-            </button>
-            <button className="button icon">
-                <Image src={heartIcon} alt="Spara jobb"></Image>
-            </button>
-            <button className="button icon">
-                <Image src={crossIcon} alt="Spara inte jobb"></Image>
-            </button>
-        </div>
-    )
-}
 
 export default function Home() {
 
@@ -37,7 +18,7 @@ export default function Home() {
             <div className="swipe-container">
                 {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} avaliableJobs={avaliableJobs} setAvaliableJobs={setAvaliableJobs}></SwipeCard>)}
             </div>
-            <Buttons />
+            <SwipeButtons />
         </div>
       </>
     )

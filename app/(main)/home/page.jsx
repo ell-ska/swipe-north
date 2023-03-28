2f961f0c0a9a0699e646d9884ebff573c393913a
Max Häggman <max.haggman@edu.cmeducations.se>
2023-03-28 09:01:44 +0200

    see more styling

--- app/(main)/home/page.jsx
+++ app/(main)/home/page.jsx
@@ -9,6 +9,22 @@ import returnIcon from 'public/icons/return.svg'
 import crossIcon from 'public/icons/cross.svg'
 import SwipeCard from './SwipeCard'
 
+export function Buttons() {
+    return (
+        <div className="buttons">
+            <button className="button icon">
+                <Image src={returnIcon} alt="Ångra"></Image>
+            </button>
+            <button className="button icon">
+                <Image src={heartIcon} alt="Spara jobb"></Image>
+            </button>
+            <button className="button icon">
+                <Image src={crossIcon} alt="Spara inte jobb"></Image>
+            </button>
+        </div>
+    )
+}
+
 export default function Home() {
 
     const [avaliableJobs, setAvaliableJobs] = useState(() => api)
@@ -18,17 +34,7 @@ export default function Home() {
             <div className="swipe-container">
                 {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} avaliableJobs={avaliableJobs} setAvaliableJobs={setAvaliableJobs}></SwipeCard>)}
             </div>
-            <div className="buttons">
-                <button className="button icon">
-                    <Image src={returnIcon} alt="Ångra"></Image>
-                </button>
-                <button className="button icon">
-                    <Image src={heartIcon} alt="Spara jobb"></Image>
-                </button>
-                <button className="button icon">
-                    <Image src={crossIcon} alt="Spara inte jobb"></Image>
-                </button>
-            </div>
+        <Buttons />
        </div>
     )
 }

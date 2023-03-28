6f4bc0c2682543422f23979366a8959f92f31a9f
ell-ska <ella@skagerstrom.se>
2023-03-28 17:20:44 +0200

    start of reset swipe

--- app/(main)/saved-jobs/page.jsx
+++ app/(main)/saved-jobs/page.jsx
@@ -1,16 +1,19 @@
-import Link from 'next/link'
-import JobCards from './Jobcard'
+'use client'
+import JobCard from './Jobcard'
+import { useAtom } from 'jotai'
+import { savedJobsAtom } from '@/app/atoms'
 
 export default function SavedJobs() {
+
+  const [savedJobs, setSavedJobs] = useAtom(savedJobsAtom)
+
   return (
     <>
       <h3 className='saved-jobs-title'>Sparade jobb</h3>
       <div className='job-card-container'>
-        <Link href='/saved-jobs/see-more'>
-          
-            <JobCards />
-          
-        </Link>
+        {savedJobs.map(job => {
+          return <JobCard></JobCard>
+        })}
       </div>
     </>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,10 @@ import { savedJobsAtom, loggedInAtom, avaliableJobsAtom } from './atoms'
 import Header from './Header'
 import SwipeButtons from './SwipeButtons'
 import SwipeCard from './SwipeCard'
+import api from './dummyApi'
 import './globalStyles/home.css'
 import './globalStyles/components/buttons.css'
+import swipeOutImage from 'public/images/swipe-out-of-cards.jpg'
 
 export default function Home() {
 
@@ -15,6 +17,21 @@ export default function Home() {
     const [savedJobs, setSavedJobs] = useAtom(savedJobsAtom)
     const [previousJob, setPreviousJob] = useState(null)
     const currentJob = avaliableJobs[avaliableJobs.length - 1]
+
+    const onReset = () => {
+        // set avaliable jobs to all jobs again EXCEPT for already saved jobs
+        console.log(api)
+        let newAvaliableJobs = []
+
+        savedJobs.map(savedJob => {
+            const test = api.filter(job => job.id === savedJob.id)
+            newAvaliableJobs = test
+        })
+
+        console.log(newAvaliableJobs)
+
+        // setAvaliableJobs(newAvaliableJobs)
+    }
 
     const onGoBack = () => {
         if (previousJob && !avaliableJobs.find(job => job.id === previousJob.id)) {
@@ -49,6 +66,20 @@ export default function Home() {
         <Header></Header>
         <div className='home'>
             <div className="swipe-container">
+                <div className="swipe swipe--out">
+                    <div className="swipe__image" style={{backgroundImage: `url(${swipeOutImage.src})`}}>
+                        <div className="swipe__overlay">
+                            <div className="swipe--out__content">
+                                <h2>Du har nu sett alla tillgängliga jobb!</h2>
+                                <h3>Men alla förtjänar en andra chans, eller hur?</h3>
+                            </div>
+                            <div className="swipe--out__footer">
+                                <button className="button" onClick={() => currentJob === undefined ? onReset() : null}>Börja om</button>
+                                <p>när du börjar om rensas din historik men du behåller dina sparade jobb</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} onSwipe={onSwipe} ></SwipeCard>)}
             </div>
             <SwipeButtons onSwipe={onSwipe} currentJob={currentJob} onGoBack={onGoBack} />

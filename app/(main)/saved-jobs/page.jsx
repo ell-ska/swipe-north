4b316a7e5c081980247d299534b4aa3ba8227b12
idaakermark <ida.akermark@edu.cmeducations.se>
2023-03-28 12:17:16 +0200

    see-more page linked

--- app/(main)/saved-jobs/page.jsx
+++ app/(main)/saved-jobs/page.jsx
@@ -15,21 +15,3 @@ export default function SavedJobs() {
     </>
   );
 }
-
-
-
-
-/*import JobCards from "./Jobcard"
-import Link from 'next/link';
-
-export default function SavedJobs() {
-    return (
-       <>
-        <h3 className='saved-jobs-title'>Sparade jobb</h3>
-        <div className='job-card-container'>
-            <JobCards></JobCards> 
-       </div>
-       </>
-    )
-}
-*/

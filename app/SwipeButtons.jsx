1965d7f6f731ea90b888e860bb069baa03da8f14
ell-ska <ella@skagerstrom.se>
2023-03-28 15:47:39 +0200

    functioning swipe buttons

--- app/SwipeButtons.jsx
+++ app/SwipeButtons.jsx
@@ -1,19 +1,19 @@
-import Image from "next/image"
+import Image from 'next/image'
 import heartIcon from 'public/icons/heart-outlined.svg'
 import returnIcon from 'public/icons/return.svg'
 import crossIcon from 'public/icons/cross.svg'
 import './globalStyles/components/buttons.css'
 
-export default function SwipeButtons() {
+export default function SwipeButtons({ onSwipe, currentJob, onGoBack }) {
     return (
         <div className="swipe-buttons">
-            <button className="button icon">
+            <button className="button icon" onClick={() => onGoBack()}>
                 <Image src={returnIcon} alt="Ångra"></Image>
             </button>
-            <button className="button icon">
+            <button className="button icon" onClick={() => onSwipe('up', currentJob.id)}>
                 <Image src={heartIcon} alt="Spara jobb"></Image>
             </button>
-            <button className="button icon">
+            <button className="button icon" onClick={() => onSwipe('down', currentJob.id)}>
                 <Image src={crossIcon} alt="Spara inte jobb"></Image>
             </button>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,57 @@
 'use client'
-// import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useAtom } from 'jotai'
+import { savedJobsAtom, loggedInAtom, avaliableJobsAtom } from './atoms'
 import Header from './Header'
 import SwipeButtons from './SwipeButtons'
-// import api from './dummyApi'
-import { avaliableJobsAtom } from './atoms'
+import SwipeCard from './SwipeCard'
 import './globalStyles/home.css'
 import './globalStyles/components/buttons.css'
-import SwipeCard from './SwipeCard'
 
 export default function Home() {
 
-    // const [avaliableJobs, setAvaliableJobs] = useState(null)
-    // useEffect(() => setAvaliableJobs(JSON.parse(localStorage.getItem('avaliable-jobs') || api)), [])
+    const [isLoggedIn] = useAtom(loggedInAtom)
     const [avaliableJobs, setAvaliableJobs] = useAtom(avaliableJobsAtom)
+    const [savedJobs, setSavedJobs] = useAtom(savedJobsAtom)
+    const [previousJob, setPreviousJob] = useState(null)
+    const currentJob = avaliableJobs[avaliableJobs.length - 1]
+
+    const onGoBack = () => {
+        if (previousJob && !avaliableJobs.find(job => job.id === previousJob.id)) {
+            setAvaliableJobs([...avaliableJobs, previousJob])
+
+            const newSavedJobs = savedJobs.filter(job => job.id !== previousJob.id)
+            setSavedJobs(newSavedJobs)
+        }
+    }
+
+    const onSwipe = (direction, id) => {
+
+        if (!isLoggedIn) {
+            window.location.pathname = '/login'
+            return
+        }
+
+        const currentJob = avaliableJobs.find(job => job.id === id)
+        
+        if (direction === 'up') {
+            setSavedJobs([...savedJobs, currentJob])
+        }
+        
+        const remainingJobs = avaliableJobs.filter(job => job.id != id)
+        setAvaliableJobs(remainingJobs)
+        setPreviousJob(currentJob)
+
+    }
 
     return (
       <>
         <Header></Header>
         <div className='home'>
             <div className="swipe-container">
-                {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} avaliableJobs={avaliableJobs} setAvaliableJobs={setAvaliableJobs}></SwipeCard>)}
+                {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} onSwipe={onSwipe} ></SwipeCard>)}
             </div>
-            <SwipeButtons />
+            <SwipeButtons onSwipe={onSwipe} currentJob={currentJob} onGoBack={onGoBack} />
         </div>
       </>
     )

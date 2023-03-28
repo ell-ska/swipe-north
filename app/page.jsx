b0bca2a60b2376d5c3d0232b6d3b068d9cc8cc41
ell-ska <ella@skagerstrom.se>
2023-03-28 11:59:59 +0200

    fix folder structure

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,22 @@ import returnIcon from 'public/icons/return.svg'
 import crossIcon from 'public/icons/cross.svg'
 import SwipeCard from './SwipeCard'
 
+export function Buttons() {
+    return (
+        <div className="buttons">
+            <button className="button icon">
+                <Image src={returnIcon} alt="Ångra"></Image>
+            </button>
+            <button className="button icon">
+                <Image src={heartIcon} alt="Spara jobb"></Image>
+            </button>
+            <button className="button icon">
+                <Image src={crossIcon} alt="Spara inte jobb"></Image>
+            </button>
+        </div>
+    )
+}
+
 export default function Home() {
 
     const [avaliableJobs, setAvaliableJobs] = useState(() => api)
@@ -18,20 +34,10 @@ export default function Home() {
       <>
         <Header></Header>
         <div className='home'>
-              <div className="swipe-container">
-                  {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} avaliableJobs={avaliableJobs} setAvaliableJobs={setAvaliableJobs}></SwipeCard>)}
-              </div>
-              <div className="buttons">
-                  <button className="button icon">
-                      <Image src={returnIcon} alt="Ångra"></Image>
-                  </button>
-                  <button className="button icon">
-                      <Image src={heartIcon} alt="Spara jobb"></Image>
-                  </button>
-                  <button className="button icon">
-                      <Image src={crossIcon} alt="Spara inte jobb"></Image>
-                  </button>
-              </div>
+            <div className="swipe-container">
+                {avaliableJobs.map(job => <SwipeCard key={job.jobTitle} {...job} avaliableJobs={avaliableJobs} setAvaliableJobs={setAvaliableJobs}></SwipeCard>)}
+            </div>
+            <Buttons />
         </div>
       </>
     )

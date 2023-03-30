91702ed4e025550f9e07a35c201b41e6db41e9bb
ell-ska <ella@skagerstrom.se>
2023-03-30 13:44:49 +0200

    add 404 page functionality

---
+++ app/not-found.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Header from './Header'
+import './globalStyles/notfound.css'
+import './globalStyles/components/buttons.css'
+
+export default function NotFound() {
+
+    return (
+        <>
+            <Header></Header>
+            <div className='not-found-page'>
+                <h3 className='not-found-title'>Oj oj oj!</h3>
+                <p className='not-found-p'>Nu har du nog gått vilse, det är dags att gå hem igen</p>
+                    <Link href='/'>
+                        <div className='button not-found-button'>Tillbaka hem</div>
+                    </Link>
+            </div>
+        </>
+    )
+}

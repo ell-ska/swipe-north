9e9401daabffb11a543cdabd1f7840bedcb72175
ell-ska <ella@skagerstrom.se>
2023-04-03 17:13:04 +0200

    clean up get filtered jobs

--- app/getFilteredJobs.js
+++ app/getFilteredJobs.js
@@ -5,35 +5,24 @@ const getFilteredJobs = (tags, savedJobs) => {
     let filteredJobs = api
     const activeTags = tags.filter(tag => tag.active).map(tag => tag.name)
 
+    // filter jobs according to tags
     if (activeTags.length > 0) {
-
         filteredJobs = api.filter(job => {
     
-            const jobTags = job.tags.map(tag => tag.name)
-    
-            return jobTags.some(jobTag => {
-    
-                const tag = tags.find(tag => tag.name == jobTag)
-    
-                return (
-                    tag && activeTags.includes(jobTag)
-                )
-    
-            })
-    
+            const currentJobTags = job.tags.map(tag => tag.name)
+            return currentJobTags.some(jobTag => activeTags.includes(jobTag))
+
         })
-
     }
-
-    let filteredJobsWithoutSavedJobs = filteredJobs
     
+    // remove already saved jobs from the newly filtered jobs
     if (savedJobs.length > 0) {
         savedJobs.map(savedJob => {
-            filteredJobsWithoutSavedJobs = filteredJobsWithoutSavedJobs.filter(job => job.id !== savedJob.id)
+            filteredJobs = filteredJobs.filter(job => job.id !== savedJob.id)
         })
     }
 
-    return filteredJobsWithoutSavedJobs
+    return filteredJobs
 
 }
 
